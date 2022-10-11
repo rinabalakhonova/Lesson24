bad1cd2dfe13198a529469bfffc4b7d63abe6901
rinabalakhonova <katia.balahonowa@yandex.ru>
2022-10-11 10:39:11 +0300

    Initial commit

---
+++ src/by/itstep/rinabalakhonava/controller/Main.java
@@ -0,0 +1,10 @@
+package by.itstep.rinabalakhonava.controller;
+
+import by.itstep.rinabalakhonava.model.entity.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Alex";
+    }
+}
